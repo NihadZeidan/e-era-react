7e2069f2db2b96bf1b628882d35a4935adec394a
Nihad Emad Zeidan <nihadzeidan@gmail.com>
2021-11-01 14:59:24 +0200

    styling transform and transition

--- src/components/menuItem/menuItem.component.jsx
+++ src/components/menuItem/menuItem.component.jsx
@@ -2,14 +2,14 @@ import "./menuItem.styles.scss";
 
 export default function MenuItem({ title, imageUrl, size }) {
   return (
-    <div
-      className={`${size} menu-item`}
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    >
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+
       <div className="content">
-        <h1 className="title"> {title} </h1>
+        <h1 className="title"> {title.toUpperCase()} </h1>
         <span className="subtitle"> Shop Now</span>
       </div>
     </div>
